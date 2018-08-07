f8312d869bd6cc669e52d506fbb57e8ed66644b0
James D. Bohrman <thepressure.bohrman@gmail.com>
2018-08-06 21:56:53 -0400

    Feature updates

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+PACKAGES = find_packages()
+
+opts = dict(name="mypackage",
+            maintainer="jonhue",
+            maintainer_email="",
+            description="Encourage, track & analyze personal growth.",
+            long_description="Encourage, track & analyze personal growth.",
+            url="https://github.com/jonhue/the_growth_app",
+            download_url="https://github.com/jonhue/the_growth_app",
+            license="MIT",
+            packages=PACKAGES)
+
+
+if __name__ == '__main__':
+    setup(**opts)
